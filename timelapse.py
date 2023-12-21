5cf06365f3731ba345c9a5b186ad753eeef8b3ff
AntonSangho <sanghoemail@gmail.com>
2023-12-21 20:44:04 +0900

    add timelapse script

---
+++ timelapse.py
@@ -0,0 +1,34 @@
+import machine 
+from machine import Pin
+import time
+import camera
+
+flash = Pin(4, Pin.OUT)
+flash.value(1)
+time.sleep(1)
+flash.value(0)
+
+uos.mount(machine.SDCard(), "/sd") # mount the SD card
+
+camera.init()
+camera.quality(12)
+camera.framesize(9) # 1024x768
+
+count = 0
+
+while True:
+    if count == 2200:
+        flash.value(1)
+        print("captured the images")
+        break
+    buf = camera.capture()
+    file = open('/sd/pics/'+str(count)+'.jpg', 'wb')
+    file.write(buf)
+    file.close()
+    print(str(count)+'.jpg is captutred')
+
+    count += 1 
+    time.sleep(1)
+
+print("Done all the work")
+
